7ce6065bbdb8c7859438baaf5a13af2df56d820f
Sanich <sansanichofficial@vk.com>
2025-01-07 18:02:24 +0300

    Update licence.js

--- js/licence.js
+++ js/licence.js
@@ -1,30 +1,36 @@
+const userAgent = navigator.userAgent;
+
+// Проверяем, содержит ли userAgent слова, указывающие на мобильное устройство
+const isMobile = /Mobi|Android/i.test(userAgent);
+
+if (isMobile) {
+    console.log('Mobile'); // Для телефонов и планшетов
+} else {
+    console.log('PC'); // Для ПК и ноутбуков
+}
 
 document.getElementById('publiclink_1').addEventListener('click', function(event) {
     event.preventDefault(); // Отменяем переход по ссылке
-     window.location.href = '/files/1.mp4'; // Переадресация
-    });
-//     const code = prompt('Содержимое защищено, введите код доступа!');
-//     const messageDiv = document.getElementById('message');
-//     messageDiv.textContent = 'Защищено паролем';
-//     if (code === null) {
-//         messageDiv.textContent = 'Действие отменено';
-//         alert('Отменено')
-//     } else if (code === '78912') {
-//         window.location.href = '/files/1.mp4'; // Переадресация
-//     } else {
-//         alert('Неверный пароль')
-//         messageDiv.textContent = 'Неверный код';
-//     }
-// });
+    window.location.href = '/files/1.mp4'; // Переадресация
+});
+
 document.getElementById('publiclink_2').addEventListener('click', function(event) {
     event.preventDefault(); // Отменяем переход по ссылке
-     window.location.href = '/files/2.mp4'; // Переадресация
-    });
+    window.location.href = '/files/2.mp4'; // Переадресация
+});
+
 document.getElementById('publiclink_3').addEventListener('click', function(event) {
     event.preventDefault(); // Отменяем переход по ссылке
-     window.location.href = '/files/ava.zip'; // Переадресация
-    });
+    if (isMobile) {
+        alert('Файл доступен только на компьютерах и ноутбуках'); // Показываем alert, если пользователь на ПК
+    }else{
+    window.location.href = '/files/ava.zip';} // Переадресация
+});
+
 document.getElementById('publiclink_4').addEventListener('click', function(event) {
     event.preventDefault(); // Отменяем переход по ссылке
-     window.location.href = '/files/cpp.zip'; // Переадресация
-    });
+    if (isMobile) {
+        alert('Файл доступен только на компьютерах и ноутбуках'); // Показываем alert, если пользователь на ПК
+    }else{
+    window.location.href = '/files/cpp.zip';} // Переадресация
+});
